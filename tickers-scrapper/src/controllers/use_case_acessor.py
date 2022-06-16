51b576b0b41fb98d2ece019e475ca61a1ca21a48
Mariana <m.alencar.v@outlook.com>
2022-06-16 10:43:28 -0300

    refactor: change in scrapper to accept new impls

---
+++ tickers-scrapper/src/controllers/use_case_acessor.py
@@ -0,0 +1,16 @@
+from ..models.accessor_interface import AccessorInterface
+from ..models.use_case_processor import UseCaseProcessor
+from  ..controllers.infomoney_use_case import InfomoneyUseCase
+
+class UseCaseAcessor(AccessorInterface):
+    use_case_instance: UseCaseProcessor = None
+
+    @staticmethod
+    def get_impl(choosen_use_case: str) -> UseCaseProcessor:
+        available_use_cases = {
+            "infomoney": InfomoneyUseCase
+        }
+
+        if (not UseCaseAcessor.use_case_instance):
+            UseCaseAcessor.use_case_instance = available_use_cases[choosen_use_case]
+        return UseCaseAcessor.use_case_instance
