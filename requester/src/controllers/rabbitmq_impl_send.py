be918007b0792ffaf7c5890f527f5b3de453e373
Mariana <m.alencar.v@outlook.com>
2022-06-16 10:41:30 -0300

    add: rabbitmq sender impl

---
+++ requester/src/controllers/rabbitmq_impl_send.py
@@ -0,0 +1,31 @@
+import json
+from ..config import config
+from ..models.messager_broker_interface_send import MessagerBrokerInterfaceSend
+from pika import BlockingConnection, ConnectionParameters
+from pika.adapters.blocking_connection import BlockingChannel
+
+class RabbitmqSend(MessagerBrokerInterfaceSend):
+    connection: BlockingConnection
+    channel: BlockingChannel
+    def __init__(self):
+        self.connection = None
+        self.channel = None
+
+    def connect_to_broker(self):
+        parameters = config('messager.ini','rabbitmq')
+        connection_parameters = ConnectionParameters(
+            host = parameters['host'],
+            port = parameters['port'],
+            virtual_host=parameters['virtual_host']
+        ) 
+        self.connection = BlockingConnection(connection_parameters)
+        self.channel = self.connection.channel()
+
+    def send_message(self, message_body: str, routingkey:str):
+        parameters = config('messager.ini','rabbitmq')
+        self.channel.basic_publish(
+            exchange=parameters['exchange'],
+            routing_key=routingkey,
+            body=message_body
+        )
+        print(f'[INFO] Sent {message_body}')
