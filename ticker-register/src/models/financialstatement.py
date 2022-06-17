6082ac0c5361387a2b4e17620459ff7e89cd7154
Mariana <m.alencar.v@outlook.com>
2022-06-16 21:58:24 -0300

    add: financial statement model and orm

--- ticker-register/src/models/financialstatement.py
+++ ticker-register/src/models/financialstatement.py
@@ -1,7 +1,7 @@
 import datetime as dt
 
 class FinancialStatement:
-    ticker_id: int
+    ticker_code: str
     statement_date: dt.date
     stocks_amount: int
     div_yield: float
@@ -15,12 +15,12 @@ class FinancialStatement:
     ebit: int
     liquid_profit: int
 
-    def __init__(self,ticker_id: int, statement_date: dt.date,
+    def __init__(self,ticker_code: str, statement_date: dt.date,
         stocks_amount: int, div_yield: float,assets: int, disponibility: int,
         current_assets: int, gross_debt: int, net_debt: int,liquid_equity: int,
         net_revenue: int, ebit: int, liquid_profit: int):
 
-        self.ticker_id = ticker_id
+        self.ticker_code = ticker_code
         self.statement_date = statement_date
         self.stocks_amount = stocks_amount
         self.div_yield = div_yield
